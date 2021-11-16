2fee087795d3b9e769e9023e75b441e03256ddd5
Ahmed awadh alqhtani <a7mdalq@MacBook-Air-Ahmed.local>
2021-11-16 09:22:50 +0300

    Completed

---
+++ Prototype-Navigation-Table-Ahmed-Alqhtani/Prototype-Navigation-Table-Ahmed-Alqhtani/Models/DetailVC.swift
@@ -0,0 +1,8 @@
+//
+//  DetailVC.swift
+//  Prototype-Navigation-Table-Ahmed-Alqhtani
+//
+//  Created by Ahmed awadh alqhtani on 10/04/1443 AH.
+//
+
+import Foundation
---
+++ Prototype-Navigation-Table-Ahmed-Alqhtani/Prototype-Navigation-Table-Ahmed-Alqhtani/Models/items.swift
@@ -0,0 +1,14 @@
+//
+//  items.swift
+//  Prototype-Navigation-Table-Ahmed-Alqhtani
+//
+//  Created by Ahmed awadh alqhtani on 10/04/1443 AH.
+//
+
+
+import Foundation
+
+struct Item {
+  let name: String
+  let description:String
+}
