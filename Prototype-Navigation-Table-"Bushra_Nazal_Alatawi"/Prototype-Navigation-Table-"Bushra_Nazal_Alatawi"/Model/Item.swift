2d659204538afc08c43106b735910f24fd37d60b
bushra nazal alatwi <bushran486gmail.com@MacBook-Pro-bushra.local>
2021-11-16 09:02:42 +0300

    Prototype-Navigation-Table

---
+++ Prototype-Navigation-Table-"Bushra_Nazal_Alatawi"/Prototype-Navigation-Table-"Bushra_Nazal_Alatawi"/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  ItemStructure.swift
+//  Prototype-Navigation-Table-"Bushra_Nazal_Alatawi"
+//
+//  Created by bushra nazal alatwi on 10/04/1443 AH.
+//
+
+import Foundation
+
+struct Item {
+  var name: String
+  var description: String
+}
