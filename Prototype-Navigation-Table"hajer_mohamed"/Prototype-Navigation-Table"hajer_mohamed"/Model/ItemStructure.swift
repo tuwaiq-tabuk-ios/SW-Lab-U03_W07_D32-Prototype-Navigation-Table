f31f8110a5fa8801daf68fd1cd8a2cbd7ee61d10
hajer117 <h117hh@hotmail.com>
2021-11-16 08:27:49 +0300

    FinshNavigationTable

---
+++ Prototype-Navigation-Table"hajer_mohamed"/Prototype-Navigation-Table"hajer_mohamed"/Model/ItemStructure.swift
@@ -0,0 +1,23 @@
+//
+//  ItemStructure.swift
+//  Prototype-Navigation-Table"hajer_mohamed"
+//
+//  Created by hajer . on 10/04/1443 AH.
+//
+
+import Foundation
+
+struct Item {
+    
+    var name : String
+    var description : String
+    
+    init(name : String , description :String) {
+        self.name = name
+        self.description=description
+        
+        
+    }
+}
+
+
