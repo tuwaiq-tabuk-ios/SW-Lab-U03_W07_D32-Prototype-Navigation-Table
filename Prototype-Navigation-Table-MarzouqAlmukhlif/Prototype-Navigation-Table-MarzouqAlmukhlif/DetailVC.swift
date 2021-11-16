f7fa8d31bcc571a72bc4bd4537e7c0cbbb9543b1
Marzouq Almukhlif <mhster_nice@Marzouqs-MacBook-Pro.local>
2021-11-16 09:34:20 +0300

    finish Prototype-Navigation-Table

---
+++ Prototype-Navigation-Table-MarzouqAlmukhlif/Prototype-Navigation-Table-MarzouqAlmukhlif/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Prototype-Navigation-Table-MarzouqAlmukhlif
+//
+//  Created by Marzouq Almukhlif on 09/04/1443 AH.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  var descriptionText: String = ""
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    descriptionLabel.text = descriptionText
+  }
+  
+  
+  
+
+
+}
+
