335f0a7dc55909a09a9b5e52a66dab3e74475b1d
Aisha Ali <aamiss1496@gmail.com>
2021-11-16 08:45:54 +0300

    Navigation Table

---
+++ ProtoType-Navigation-Tabel-Aisha Ali/ProtoType-Navigation-Tabel-Aisha Ali/Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ProtoType-Navigation-Tabel-Aisha Ali
+//
+//  Created by Aisha Ali on 11/15/21.
+//
+
+import Foundation
+
+
+struct  Item {
+  let name: String
+  let description :String
+//  init(name:String, description:String) {
+//    self.name = name
+//    self.description = description
+//  }
+}
