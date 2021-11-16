cf5b883c885c2714c55ae4f237efab148378e9a4
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-11-16 08:28:46 +0300

    Create Table

---
+++ Prototype-Navigation-Table-ShuruqAlanazi/Prototype-Navigation-Table-ShuruqAlanazi/Controllers/DetailVC.swift
@@ -0,0 +1,8 @@
+//
+//  DetailVC.swift
+//  Prototype-Navigation-Table-ShuruqAlanazi
+//
+//  Created by Shorouq AlAnzi on 10/04/1443 AH.
+//
+
+import Foundation
---
+++ Prototype-Navigation-Table-ShuruqAlanazi/Prototype-Navigation-Table-ShuruqAlanazi/Models/Items.swift
@@ -0,0 +1,20 @@
+//
+//  Items.swift
+//  Prototype-Navigation-Table-ShuruqAlanazi
+//
+//  Created by Shorouq AlAnzi on 10/04/1443 AH.
+//
+
+import Foundation
+
+struct Items {
+  
+  let name : String
+  let description : String
+  
+  
+//  init(name : String , description: String) {
+//    self.name = name
+//    self.description = description
+//  }
+}
